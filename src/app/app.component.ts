9d206c1dfafc5ecdb0101d18375615835f8d94d3
natalia-st <nastgm55@gmail.com>
2023-12-10 14:35:46 +0100

    footer and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, ],
+  imports: [RouterOutlet, RouterLink, FooterComponent, HeaderComponent ],
   styleUrls: ['./app.component.css'], 
   templateUrl: './app.component.html'
 })
