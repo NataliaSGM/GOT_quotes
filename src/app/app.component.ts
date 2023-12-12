c93703e84f63210132ec2577bd70618ed712a691
natalia-st <nastgm55@gmail.com>
2023-12-12 12:07:40 +0100

    update routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HousesComponent } from './houses/houses.component';
+import { PersonsComponent } from './persons/persons.component';
+import { CharacterComponent } from './character/character.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, FooterComponent, HeaderComponent ],
+  imports: [RouterOutlet, RouterLink, FooterComponent, HeaderComponent, HousesComponent, PersonsComponent, CharacterComponent, HousesComponent, QuotesComponent],
   styleUrls: ['./app.component.css'], 
   templateUrl: './app.component.html'
 })
